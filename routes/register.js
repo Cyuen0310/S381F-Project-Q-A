9f6c663940cea3129cc4efe9784d27f185aa175c
Cyuen0310 <chanszeyuen0310@icloud.com>
2023-11-16 06:40:12 +0000

    main

--- routes/register.js
+++ routes/register.js
@@ -16,11 +16,8 @@ router.get("/", (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
-    // Check if the email is already registered
     const existingUser = await User.findOne({ name: req.body.name });
     const existingemail = await User.findOne({ email: req.body.email });
-
-    
 
     if (existingUser) {
       return res.render("register", {
@@ -45,14 +42,12 @@ router.post("/", async (req, res) => {
       });
     }
 
-    // Create a new user
     const user = new User({
       name: req.body.name,
       password: req.body.password,
       email : req.body.email,
     });
 
-    // Save the user to the database
     await user.save();
 
     res.redirect("/login");
